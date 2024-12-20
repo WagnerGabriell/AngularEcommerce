0972d4234508d37a68fa6f7c9502b0bd5f92aa93
Wagner Gabriel <wagnergoliveira@hotmail.com>
2024-12-19 23:32:22 -0300

    Criação do component navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"produto", component: ProdutosComponent},
+];
---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-produtos',
+  imports: [NavbarComponent],
+  templateUrl: './produtos.component.html',
+  styleUrl: './produtos.component.css'
+})
+export class ProdutosComponent {
+
+}
